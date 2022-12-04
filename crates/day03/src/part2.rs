5c5ed33daea85aed15a7f6ede310235c0de4cbd7
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2022-12-04 08:58:13 +0100

    Day03 part2 refactored

--- crates/day03/src/part2.rs
+++ crates/day03/src/part2.rs
@@ -2,24 +2,19 @@ use crate::priority::priority;
 
 pub fn solve(input: &str) {
     let lines = input.lines().collect::<Vec<&str>>();
-    let mut index = 0;
-    let mut sum = 0;
 
-    while index < lines.len() {
-        let line0 = lines[index];
-        let line1 = lines[index + 1];
-        let line2 = lines[index + 2];
+    let sum = lines
+        .chunks(3)
+        .map(|chunk| {
+            let c = chunk[0]
+                .chars()
+                .filter(|item| chunk[1].contains(*item) && chunk[2].contains(*item))
+                .next()
+                .unwrap();
 
-        let c = line0
-            .chars()
-            .filter(|item| line1.contains(*item) && line2.contains(*item))
-            .next()
-            .unwrap();
-
-        sum += priority(&c);
-
-        index = index + 3;
-    }
+            priority(&c)
+        })
+        .sum::<usize>();
 
     println!("Part 2 solution: {}", sum);
 }
